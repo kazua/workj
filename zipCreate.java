21503107688c8ab0f41318578b1bf2a54f9c0493
kazua <loveisover777@gmail.com>
2013-04-24 23:06:35 +0900

    圧縮用プログラム（階層対応）

--- zipCreate.java
+++ zipCreate.java
@@ -8,8 +8,7 @@ import org.apache.tools.zip.ZipEntry;
 import org.apache.tools.zip.ZipOutputStream;
 
 public class zipCreate {
-	private static void zipFileMake(String dir, String zipfile)
-			throws Exception {
+	private void zipFileMake(String dir, String zipfile) throws Exception {
 		ZipOutputStream zo = new ZipOutputStream(new FileOutputStream(zipfile));
 		zo.setEncoding("MS932");
 		File tgtdir = new File(dir);
@@ -20,8 +19,8 @@ public class zipCreate {
 		zo.flush();
 		zo.close();
 	}
-	private static void zipFileMakeProc(ZipOutputStream zo, File path,
-			String hrc) throws Exception {
+	private void zipFileMakeProc(ZipOutputStream zo, File path, String hrc)
+			throws Exception {
 		if (path.isDirectory()) {
 			File[] files = path.listFiles();
 			for (int i = 0; i < files.length; i++) {
@@ -46,7 +45,8 @@ public class zipCreate {
 		try {
 			SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
 			Date now = new Date();
-			zipFileMake("D:/写真", "E:/backup/家族写真" + sdf.format(now) + ".zip");
+			zipCreate zc = new zipCreate();
+			zc.zipFileMake("D:/写真", "E:/backup/家族写真" + sdf.format(now) + ".zip");
 		} catch (Exception e) {
 			System.out.println(e.toString());
 			if (e.getCause() != null)
