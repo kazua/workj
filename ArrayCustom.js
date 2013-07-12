2e54d0e24907b5b52c2a8a5c3895ac4f198ea5a6
kazua <loveisover777@gmail.com>
2013-07-13 00:24:54 +0900

    Arrayのcontainsとdistinct

---
+++ ArrayCustom.js
@@ -0,0 +1,28 @@
+//write kazua
+
+if (!Array.prototype.contains) {
+	Array.prototype.contain = function(value) {
+		for ( var i = 0; i < this.length; i++)
+			if (this[i] === value)
+				return true;
+		return false;
+	};
+}
+if (!Array.prototype.distinct) {
+	Array.prototype.distinct = function() {
+		var t = Object(this), nt = [], len = t.length >>> 0;
+		if (len == 0)
+			return this;
+		for ( var i = 0; i < t.length; i++)
+			if (!nt.contain(t[i]))
+				nt.push(t[i]);
+
+		return nt;
+	};
+}
+
+function test() {
+	var a = new Array("1", "3", "4", "1", "5");
+	a = a.distinct();
+	alert(a);
+}
